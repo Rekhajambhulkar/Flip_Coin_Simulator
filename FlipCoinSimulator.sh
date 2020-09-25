f648609f43a76447f0d273f32624efee9a19b299
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-25 20:36:11 +0530

    How much time head and tail has won

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -2,9 +2,15 @@
 
 echo "WELCOME in FlipCoinSimulator"
 
+flip=1
+
 #Flip a coin and check its Head or tail
 
-choice=$((RANDOM%2))
+function Flip()
+{
+	choice=$((RANDOM%2))
+	echo $choice
+}
 
 if [ $choice -eq 1 ]
 then
@@ -13,4 +19,21 @@ else
 	echo "Head"
 fi
 
+head=0
+tail=0
+choice=20
 
+for((i=1;i<=$choice;i++))
+do
+if [ $res == $flip ]
+then
+        ((tail++))
+else
+        ((head++))
+fi
+        res="$(Flip)"
+done
+
+
+echo "Total head:" $head
+echo "Total tail:" $tail
